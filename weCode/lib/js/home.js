d42e7a1162b4a0f7d7e0c6f183b29ad149f8c6c3
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2020-04-10 14:08:31 +0200

    test

--- weCode/lib/js/home.js
+++ weCode/lib/js/home.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker();
+    $('.fixed-action-btn').floatingActionButton({
+        hoverEnabled: false
+    });
+    $('.tooltipped').tooltip({
+        position: 'left'
+    });
+    $('.scrollspy').scrollSpy();
 });
